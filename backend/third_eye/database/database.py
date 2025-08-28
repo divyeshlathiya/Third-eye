25ba97f5b7d6d6d2eaaf9c509b6043ece4524145
divyeshlathiya <dev202lathiya@gmail.com>
2025-08-29 00:35:23 +0530

    Setup postgreSQL connection

--- backend/third_eye/database/database.py
+++ backend/third_eye/database/database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./auth.db"
+load_dotenv()
+
+# DATABASE_URL = "sqlite:///./auth.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(url=DATABASE_URL, connect_args={
                        "check_same_thread": False})
