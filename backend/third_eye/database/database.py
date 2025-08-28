f51c5632bb16ceaffef9f3f98924aa77d23dfa46
divyeshlathiya <dev202lathiya@gmail.com>
2025-08-29 00:49:10 +0530

    Fix postgreSQL config error

--- backend/third_eye/database/database.py
+++ backend/third_eye/database/database.py
@@ -8,8 +8,13 @@ load_dotenv()
 # DATABASE_URL = "sqlite:///./auth.db"
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(url=DATABASE_URL, connect_args={
-                       "check_same_thread": False})
+# Detect SQLite
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
